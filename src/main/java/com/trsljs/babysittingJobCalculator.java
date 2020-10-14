74c3bd1fff551b01c010202249d5158ad141152f
tr-smith <timothyrocksmith@gmail.com>
2020-10-14 16:15:01 -0400

    project start

--- src/main/java/com/trsljs/babysittingJobCalculator.java
+++ src/main/java/com/trsljs/babysittingJobCalculator.java
@@ -4,7 +4,7 @@ package com.trsljs;
  * Hello world!
  *
  */
-public class App 
+public class babysittingJobCalculator
 {
     public static void main( String[] args )
     {
--- src/test/java/com/trsljs/babysittingJobCalculatorTest.java
+++ src/test/java/com/trsljs/babysittingJobCalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class babysittingJobCalculatorTest
 {
     /**
      * Rigorous Test :-)
