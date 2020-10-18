d25eadafd03592080f724216a16f29771d46b253
tr-smith <timothyrocksmith@gmail.com>
2020-10-18 12:33:18 -0400

    Finished javaDocs and added jar configuration to Maven pom

--- src/main/java/com/trsljs/BabysittingJobCalculator.java
+++ src/main/java/com/trsljs/BabysittingJobCalculator.java
@@ -12,8 +12,9 @@ import java.util.Objects;
 import java.util.Vector;
 
 /**
- * Hello world!
+ * Main Class and GUI for the Babysitting Job Calculator
  *
+ * @author <a href="mailto:timothyrocksmith@gmail.com">Tim Smith</a>
  */
 public class BabysittingJobCalculator extends JFrame implements ActionListener {
     private JComboBox<String> startTimeInput;
@@ -73,6 +74,9 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * Creates and populates the GUI Components
+     */
     private void initComponents() {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
@@ -131,6 +135,9 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         pack();
     }
 
+    /**
+     * Resets all fields and variable to the initial state
+     */
     private void resetCalculator() {
         resultMessage.setText("");
         startTimeInput.setSelectedIndex(0);
@@ -141,6 +148,13 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         calcButton.setEnabled(false);
     }
 
+    /**
+     * Creates a {@link JLabel} with common settings for consistency in the look and feel
+     *
+     * @param labelText the {@link JLabel#getText()} to display on the label.
+     * @param input the {@link JComboBox<String>} to associate the label with.
+     * @return the configured {@link JLabel}
+     */
     private JLabel createInputLabel(String labelText, JComboBox<String> input) {
         JLabel label = new JLabel(labelText);
         Dimension dimension = new Dimension(100, 25);
@@ -154,6 +168,12 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         return label;
     }
 
+    /**
+     * Creates a {@link JComboBox} with common settings for consistency in the look and feel
+     *
+     * @param model the {@link Vector<String>} to populate the combobox model with.
+     * @return the configured {@link JComboBox<String>}
+     */
     private JComboBox<String> createCombobox(Vector<String> model) {
         JComboBox<String > comboBox = new JComboBox<>(model);
         Dimension dimension = new Dimension(100, 25);
@@ -164,6 +184,12 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         return comboBox;
     }
 
+    /**
+     * Creates a {@link JButton} with common settings for consistency in the look and feel
+     *
+     * @param text the {@link JButton#getText()} to display on the button.
+     * @return the configured {@link JButton}
+     */
     private JButton createButton(String text) {
         JButton button = new JButton(text);
         Dimension dimension = new Dimension(125, 30);
@@ -175,6 +201,9 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         return button;
     }
 
+    /**
+     * Retrieve the selected time values and pass the to the {@link CalculatorService} to calculate total job charge.
+     */
     private void performCalculations() {
         LocalTime startTime = LocalTime.parse((String) Objects.requireNonNull(startTimeInput.getSelectedItem()), TimeUtilities.TIME_FORMATTER);
         LocalTime bedTime = LocalTime.parse((String) Objects.requireNonNull(bedTimeInput.getSelectedItem()), TimeUtilities.TIME_FORMATTER);
@@ -183,7 +212,9 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         resultMessage.setText(calculatorService.calculateJobTotal(startTime, bedTime, endTime));
     }
 
-
+    /**
+     * Enables the calculate button iff all times have been selected and valid.
+     */
     private void enableCalculate() {
         if (startTimeInput.getSelectedIndex() > 0 && bedTimeInput.getSelectedIndex() > 0 &&
                 endTimeInput.getSelectedIndex() > 0) {
@@ -193,6 +224,9 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * Checks the start time selection to make sure it does not exceed the end time selection if selection exists.
+     */
     private void validateStartSelection() {
         if (startTimeInput.getSelectedIndex() > 0 && endTimeInput.getSelectedIndex() > 0) {
             LocalTime startTime = LocalTime.parse((String) Objects.requireNonNull(startTimeInput.getSelectedItem()), TimeUtilities.TIME_FORMATTER);
@@ -208,6 +242,9 @@ public class BabysittingJobCalculator extends JFrame implements ActionListener {
         enableCalculate();
     }
 
+    /**
+     * Checks the end time selection to make sure it is equal to or later than the start time selection if selection exists.
+     */
     private void validateEndSelection() {
         if (startTimeInput.getSelectedIndex() > 0 && endTimeInput.getSelectedIndex() > 0) {
             LocalTime startTime = LocalTime.parse((String) Objects.requireNonNull(startTimeInput.getSelectedItem()), TimeUtilities.TIME_FORMATTER);
